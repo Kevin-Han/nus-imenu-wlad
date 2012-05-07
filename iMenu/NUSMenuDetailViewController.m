06dc0bd8b232d7fe0793bff44011ae6959bea5e6
naychizaw@gmail.com <naychizaw@gmail.com@9a5b676d-afe6-9575-3770-0da27b9d0e03>
2012-05-07 15:57:04 +0000

    Add sending email

--- iMenu/NUSMenuDetailViewController.m
+++ iMenu/NUSMenuDetailViewController.m
@@ -76,6 +76,92 @@
 {
     NSLog(@"%s Calories=%@", __FUNCTION__, [_dataItem objectForKey:@"Calories"]);
     
+    
+    if ([MFMailComposeViewController canSendMail])
+    {
+        MFMailComposeViewController *mailer = [[MFMailComposeViewController alloc] init];
+        
+        mailer.mailComposeDelegate = self;
+        
+        [mailer setSubject:@"Here is the good deal."];
+        
+       // NSArray *toRecipients = [NSArray arrayWithObjects:@"fisrtMail@example.com", @"secondMail@example.com", nil;
+       // [mailer setToRecipients:toRecipients];
+        
+       // UIImage *myImage = [UIImage imageNamed:@"icon.png"];
+      //  UIImage *myImage = [UIImage imageNamed:[_dataItem objectForKey:@"Image"]];
+        //UIImage *myImage = [_dataItem objectForKey:@"Image"];
+      //  NSData *imageData = UIImagePNGRepresentation(myImage);
+       // [mailer addAttachmentData:imageData mimeType:@"image/png" fileName:@"mobiletutsImage"];	
+        
+        
+        NSString *emailBody = @"<html><head></head><body><table><tr><td colspan=\"2\">";
+        emailBody = [emailBody stringByAppendingFormat:@"%@", [_dataItem objectForKey:@"Name"]];
+        emailBody = [emailBody stringByAppendingString:@"</td></tr><tr><td>Price:</td><td>"];
+        emailBody = [emailBody stringByAppendingFormat:@"%@", [_dataItem objectForKey:@"Price"]];
+        emailBody = [emailBody stringByAppendingString:@"</td></tr><tr><td>Calories</td><td>"];
+        emailBody = [emailBody stringByAppendingFormat:@"%@", [_dataItem objectForKey:@"Calories"]];
+        emailBody = [emailBody stringByAppendingString:@"</td></tr><tr><td>Dietary Fiber</td><td>"];
+        emailBody = [emailBody stringByAppendingFormat:@"%@", [_dataItem objectForKey:@"Dietary Fiber"]];
+        emailBody = [emailBody stringByAppendingString:@"</td></tr><tr><td>Sugars</td><td>"];
+        emailBody = [emailBody stringByAppendingFormat:@"%@", [_dataItem objectForKey:@"Sugars"]];
+        emailBody = [emailBody stringByAppendingString:@"</td></tr><tr><td>Total Fats</td><td>"];
+        emailBody = [emailBody stringByAppendingFormat:@"%@", [_dataItem objectForKey:@"Total Fats"]];
+        emailBody = [emailBody stringByAppendingString:@"</td></tr><tr><td>Total Carbohydrate</td><td>"];
+        emailBody = [emailBody stringByAppendingFormat:@"%@", [_dataItem objectForKey:@"Total Carbohydrate"]];
+        emailBody = [emailBody stringByAppendingString:@"</td></tr><tr><td>Amount Serving Size</td><td>"];
+        emailBody = [emailBody stringByAppendingFormat:@"%@", [_dataItem objectForKey:@"Amount Serving Size"]];
+        emailBody = [emailBody stringByAppendingString:@"</table></body></html>"];
+        
+        [mailer setMessageBody:emailBody isHTML:YES];
+        
+        // only for iPad
+        // mailer.modalPresentationStyle = UIModalPresentationPageSheet;
+        
+        [self presentModalViewController:mailer animated:YES];
+        
+        //[mailer release];
+    }
+    else
+    {
+        [self printMessage:@"cannot SendMail"];
+    }
+    
+}
+
+- (void)mailComposeController:(MFMailComposeViewController*)controller didFinishWithResult:(MFMailComposeResult)result error:(NSError*)error 
+{	
+	switch (result)
+	{
+		case MFMailComposeResultCancelled:
+			NSLog(@"Mail cancelled: you cancelled the operation and no email message was queued");
+			break;
+		case MFMailComposeResultSaved:
+			NSLog(@"Mail saved: you saved the email message in the Drafts folder");
+			break;
+		case MFMailComposeResultSent:
+			NSLog(@"Mail send: the email message is queued in the outbox. It is ready to send the next time the user connects to email");
+			break;
+		case MFMailComposeResultFailed:
+			NSLog(@"Mail failed: the email message was nog saved or queued, possibly due to an error");
+			break;
+		default:
+			NSLog(@"Mail not sent");
+			break;
+	}
+    
+	[self dismissModalViewControllerAnimated:YES];
+}
+
+-(void) printMessage:(NSString *)name{
+    UIAlertView *alertPopUp=[[UIAlertView alloc]
+                             initWithTitle:@"Alert"
+                             message:name 
+                             delegate:nil 
+                             cancelButtonTitle:@"OK"
+                             otherButtonTitles:nil];
+    [alertPopUp show];
+    NSLog(@"The message is %@",name);
 }
 
 @end
